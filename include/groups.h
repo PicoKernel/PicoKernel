b1a89b8ae0d888a291d7d8568420046081835a1c
datenbar <datenbar@tutamail.com>
2026-04-26 01:38:23 +0530

    chore: Add Doxyfile and basic group definitions

---
+++ include/groups.h
@@ -0,0 +1,27 @@
+/**
+ * @file groups.h
+ * @brief Doxygen group definitions for PicoKernel.
+ *
+ * This file contains no code. It exists solely to define
+ * documentation groups for the Doxygen output.
+ *
+ * Do not include this file in any source file.
+ */
+
+/**
+ * @defgroup kernel Kernel
+ * @brief Core kernel subsystem.
+ *        Maintained by datenbar and rootmnt.
+ *
+ * @defgroup drivers Drivers
+ * @brief Hardware driver layer.
+ *        Maintained by ishanndas21-detroid and krishna-stone.
+ *
+ * @defgroup modules Modules
+ * @brief Kernel modules.
+ *        Maintained by Amarcode.
+ *
+ * @defgroup interface Interface
+ * @brief Web and user interface layer.
+ *        Maintained by shresth136.
+ */
