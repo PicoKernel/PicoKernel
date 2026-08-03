c224eb991a1fa6f62a1481f57327bacabe797b86
PicoKernel-Project <picokernel@modprobe.dev>
2026-08-03 15:54:37 +0000

    chore: bump version to 0.4.0-beta

--- include/version.h
+++ include/version.h
@@ -2,6 +2,6 @@
  * SPDX-FileCopyrightText: 2025 PicoKernel Contributors
  */
 #define PICOKERNEL_VERSION_MAJOR 0
-#define PICOKERNEL_VERSION_MINOR 3
+#define PICOKERNEL_VERSION_MINOR 4
 #define PICOKERNEL_VERSION_PATCH 0
-#define PICOKERNEL_VERSION_STR   "0.3.0-alpha"
+#define PICOKERNEL_VERSION_STR   "0.4.0-beta"
